9d62308386489acf47675a6311224988b7bf640a
Aniket Singh <singhaniketkumar882@gmail.com>
2025-12-23 21:18:21 +0530

    added development.json and coded in owners routers

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,9 +1,32 @@
 const express = require('express')
 const router = express.Router()
+const ownerModel = require("../models/owners-model")
+
+if(process.env.NODE_ENV == "development"){
+   router.post(("/create") , async (req,res)=>{
+    let owners = await ownerModel.find()
+    if(owners.length>0){
+        return res
+        .status(502)
+        .send("dont have permission to create owner")
+    }
+    let {fullname ,email, password} = req.body
+    let createdOwner = await ownerModel.create({
+        fullname,
+        email,
+        password,
+    })
+
+    res.send(createdOwner)
+   
+})
+
+}
 
 router.get(("/") , (req,res)=>{
     res.send("Hey")
 })
-
-
+console.log(process.env.NODE_ENV) 
+    
+    
 module.exports = router
